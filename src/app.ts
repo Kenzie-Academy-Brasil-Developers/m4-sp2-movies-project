a0c6441d944e5f231cdac937433eeb5203c5e5a7
Vitor Galdino <httpvittor@gmail.com>
2023-02-01 19:48:17 -0300

    feat: call connection with database

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import { Application } from 'express';
+import { startDatabase } from './database';
 import express = require('express');
 
 const app: Application = express();
 app.use(express.json());
 
-app.listen('3000', () => {
+app.listen('3000', async () => {
+    await startDatabase();
     console.log('Server is running on http://localhost:3000/ 🚀');
 });
