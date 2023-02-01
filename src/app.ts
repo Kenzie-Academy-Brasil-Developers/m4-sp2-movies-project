487de09c170ff5534c922e78e7e969fda8cf269b
Vitor Galdino <httpvittor@gmail.com>
2023-02-01 19:51:26 -0300

    feat: created post route

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import { Application } from 'express';
 import { startDatabase } from './database';
+import { createMovie } from './functions';
 import express = require('express');
 
 const app: Application = express();
 app.use(express.json());
+
+app.post('/movies', createMovie);
 
 app.listen('3000', async () => {
     await startDatabase();
