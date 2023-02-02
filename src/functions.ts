0ea1c2f615f4ff37d9cfde71de624c13b53826e6
Vitor Galdino <httpvittor@gmail.com>
2023-02-02 09:17:11 -0300

    feat: created new response return with new data object

--- src/functions.ts
+++ src/functions.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express';
 import { QueryConfig } from 'pg';
 
 import { client } from './database';
-import { IMovie, IMoviesRequest, MovieResult } from './interfaces';
+import { IMovie, IMoviePages, IMoviesRequest, MovieResult } from './interfaces';
 
 import format = require('pg-format');
 
@@ -27,8 +27,9 @@ export const createMovie = async (req: Request, res: Response): Promise<Response
 };
 
 export const listMovies = async (req: Request, res: Response): Promise<Response> => {
-    const perPage: any = !Number(req.query.perPage) || Number(req.query.perPage) <= 0 || Number(req.query.perPage) > 5 ? 5 : Number(req.query.perPage);
-    const page: any = !Number(req.query.page) || Number(req.query.page) <= 0 ? 0 : (Number(req.query.page) - 1) * perPage;
+    const perPage: number = !Number(req.query.perPage) || Number(req.query.perPage) <= 0 || Number(req.query.perPage) > 5 ? 5 : Number(req.query.perPage);
+    const page: number = !Number(req.query.page) || Number(req.query.page) <= 0 ? 0 : (Number(req.query.page) - 1) * perPage;
+    const currentPage: number = !Number(req.query.page) || Number(req.query.page) <= 0 ? 1 : Number(req.query.page);
 
     const queryString: string = `
         SELECT
@@ -44,6 +45,12 @@ export const listMovies = async (req: Request, res: Response): Promise<Response>
     };
 
     const queryResult: MovieResult = await client.query(queryConfig);
+    const newDataResult: IMoviePages = {
+        previousPage: currentPage === 1 ? null : `http://localhost:3000/movies?page=${currentPage - 1}&perPage=${perPage}`,
+        nextPage: `http://localhost:3000/movies?page=${currentPage + 1}&perPage=${perPage}`,
+        count: queryResult.rowCount,
+        data: queryResult.rows
+    };
 
-    return res.status(200).json(queryResult.rows);
+    return res.status(200).json(newDataResult);
 };
