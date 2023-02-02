fb657ca8978f0a83f0b7f81b442d6b983020758c
Vitor Galdino <httpvittor@gmail.com>
2023-02-01 22:21:42 -0300

    feat: created get route to list movies from database

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,14 @@
 import { Application } from 'express';
 import { startDatabase } from './database';
-import { createMovie } from './functions';
+import { createMovie, listMovies } from './functions';
+import { checkMovieName } from './middlewares';
 import express = require('express');
 
 const app: Application = express();
 app.use(express.json());
 
-app.post('/movies', createMovie);
+app.post('/movies', checkMovieName, createMovie);
+app.get('/movies', listMovies);
 
 app.listen('3000', async () => {
     await startDatabase();
