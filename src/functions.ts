833b8a2ebe238f3b0bb68736c308a515d5adac86
Vitor Galdino <httpvittor@gmail.com>
2023-02-01 21:30:20 -0300

    feat: created logic structure to "createMovie" post request

--- src/functions.ts
+++ src/functions.ts
@@ -1,5 +1,24 @@
 import { Request, Response } from 'express';
+import { client } from './database';
+import { IMovie, IMoviesRequest, MovieResult } from './interfaces';
+import format = require('pg-format');
 
-export const createMovie = (req: Request, res: Response): Response => {
+export const createMovie = async (req: Request, res: Response): Promise<Response> => {
+    const requestedData: IMoviesRequest = req.body;
 
+    const queryString: string = format(`
+        INSERT INTO
+            movies_table(%I)
+        VALUES
+            (%L)
+        RETURNING *;
+    `,
+        Object.keys(requestedData),
+        Object.values(requestedData)
+    );
+
+    const queryResult: MovieResult = await client.query(queryString);
+    const newIncludedMovie: IMovie = queryResult.rows[0];
+
+    return res.status(201).json(newIncludedMovie);
 };
