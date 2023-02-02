358aa207c069b9bec363a257d09ccf790c43cdd2
Vitor Galdino <httpvittor@gmail.com>
2023-02-02 08:53:57 -0300

    feat: created pagination to list movies

--- src/functions.ts
+++ src/functions.ts
@@ -1,6 +1,9 @@
 import { Request, Response } from 'express';
+import { QueryConfig } from 'pg';
+
 import { client } from './database';
 import { IMovie, IMoviesRequest, MovieResult } from './interfaces';
+
 import format = require('pg-format');
 
 export const createMovie = async (req: Request, res: Response): Promise<Response> => {
@@ -24,14 +27,23 @@ export const createMovie = async (req: Request, res: Response): Promise<Response
 };
 
 export const listMovies = async (req: Request, res: Response): Promise<Response> => {
+    const perPage: any = !Number(req.query.perPage) || Number(req.query.perPage) <= 0 || Number(req.query.perPage) > 5 ? 5 : Number(req.query.perPage);
+    const page: any = !Number(req.query.page) || Number(req.query.page) <= 0 ? 0 : (Number(req.query.page) - 1) * perPage;
+
     const queryString: string = `
         SELECT
             *
         FROM
             movies_table
+        LIMIT $1 OFFSET $2;
     `;
 
-    const queryResult: MovieResult = await client.query(queryString);
+    const queryConfig: QueryConfig = {
+        text: queryString,
+        values: [perPage, page]
+    };
+
+    const queryResult: MovieResult = await client.query(queryConfig);
 
     return res.status(200).json(queryResult.rows);
 };
