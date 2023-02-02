6a3d0f5765e2410074574f38497d3dc661c38fb0
Vitor Galdino <httpvittor@gmail.com>
2023-02-02 16:55:10 -0300

    feat: created sort filter to list movies

--- src/functions.ts
+++ src/functions.ts
@@ -1,5 +1,4 @@
 import { Request, Response } from 'express';
-import { QueryConfig } from 'pg';
 
 import { client } from './database';
 import { IMovie, IMoviePages, IMoviesRequest, MovieResult } from './interfaces';
@@ -26,25 +25,47 @@ export const createMovie = async (req: Request, res: Response): Promise<Response
     return res.status(201).json(newIncludedMovie);
 };
 
+const sortMoviesList = (req: Request, res: Response): Array<string> => {
+    const requiredSorts = ['price', 'duration'];
+    const requiredOrder = ['ASC', 'DESC'];
+    const sortValue = req.query.sort.toString().toLowerCase();
+    const orderValue = req.query.order?.toString().toUpperCase() || 'ASC';
+
+    if (requiredSorts.some(str => str === sortValue)) {
+        if (requiredOrder.some(str => str === orderValue)) {
+            return [sortValue, orderValue];
+        }
+    }
+
+    return ['id', 'ASC'];
+};
+
 export const listMovies = async (req: Request, res: Response): Promise<Response> => {
     const perPage: number = !Number(req.query.perPage) || Number(req.query.perPage) <= 0 || Number(req.query.perPage) > 5 ? 5 : Number(req.query.perPage);
     const page: number = !Number(req.query.page) || Number(req.query.page) <= 0 ? 0 : (Number(req.query.page) - 1) * perPage;
     const currentPage: number = !Number(req.query.page) || Number(req.query.page) <= 0 ? 1 : Number(req.query.page);
+    const sortResult: Array<string> = [];
 
-    const queryString: string = `
+    if (req.query.sort) {
+        sortResult.push(...sortMoviesList(req, res));
+    }
+
+    const queryString: string = format(`
         SELECT
             *
         FROM
             movies_table
-        LIMIT $1 OFFSET $2;
-    `;
+        ORDER BY %I %s
+        LIMIT %s OFFSET %s;
+    `,
+        sortResult[0],
+        sortResult[1],
+        perPage,
+        page
+    );
 
-    const queryConfig: QueryConfig = {
-        text: queryString,
-        values: [perPage, page]
-    };
+    const queryResult: MovieResult = await client.query(queryString);
 
-    const queryResult: MovieResult = await client.query(queryConfig);
     const newDataResult: IMoviePages = {
         previousPage: currentPage === 1 ? null : `http://localhost:3000/movies?page=${currentPage - 1}&perPage=${perPage}`,
         nextPage: `http://localhost:3000/movies?page=${currentPage + 1}&perPage=${perPage}`,
